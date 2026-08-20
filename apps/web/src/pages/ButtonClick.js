bed1a392479a9c1ffb06237cb9c9267c3439d981
ARITRA ADAK <aritra1adak@gmail.com>
2026-08-20 20:09:52 +0530

    Adding all in Demo.tsx

--- apps/web/src/pages/ButtonClick.js
+++ apps/web/src/pages/ButtonClick.js
@@ -1,9 +1,1 @@
-<button onClick={() => navigate("/guest-login")}>
-  Continue as Guest
-</button>
 
-const isGuest = localStorage.getItem("isGuest");
-
-if (isGuest === "true") {
-  console.log("User is logged in as Guest");
-}
