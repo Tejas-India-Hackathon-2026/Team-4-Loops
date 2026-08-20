15ef43e66581a36cad6ae847d62be0b74603b381
ARITRA ADAK <aritra1adak@gmail.com>
2026-08-20 20:12:40 +0530

    update routing setup from Demo.tsx

--- apps/web/src/pages/Demo.tsx
+++ apps/web/src/pages/Demo.tsx
@@ -1,17 +1,1 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import GuestLogin from "./pages/GuestLogin";
-import Home from "./pages/Home";
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/guest-login" element={<GuestLogin />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default App;
